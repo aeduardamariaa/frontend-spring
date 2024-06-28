ac74737b43487b0e4f61c7182ccecdbd66e2cd68
Maria Eduarda da Silva <silvamariaeduarda92@gmail.com>
2024-06-28 14:02:05 -0300

    Feito confirmações de js na recuperação de senha

--- src/script/password.js
+++ src/script/password.js
@@ -1,44 +1,73 @@
-function isGood(password) {
-    var password_strength = document.getElementById("password-text");
+// Adicione isto ao seu arquivo password.js
+document.addEventListener("DOMContentLoaded", function() {
+    const passwordInput = document.getElementById("password");
+    const repeatPasswordInput = document.getElementById("repeat_password");
+    const matchText = document.getElementById("match-text");
 
-    //TextBox left blank.
-    if (password.length == 0) {
-      password_strength.innerHTML = "";
-      return;
+    passwordInput.addEventListener("input", function() {
+        isGood(passwordInput.value);
+    });
+
+    repeatPasswordInput.addEventListener("input", function() {
+        checkPasswordMatch();
+    });
+
+    function isGood(password) {
+        var password_strength = document.getElementById("password-text");
+
+        if (password.length == 0) {
+            password_strength.innerHTML = "";
+            return;
+        }
+
+        var regex = new Array();
+        regex.push("[A-Z]"); // Uppercase Alphabet.
+        regex.push("[a-z]"); // Lowercase Alphabet.
+        regex.push("[0-9]"); // Digit.
+        regex.push("[$@$!%*#?&]"); // Special Character.
+
+        var passed = 0;
+
+        // Check for length
+        if (password.length >= 8) {
+            passed++;
+        }
+
+        // Check each regex
+        for (var i = 0; i < regex.length; i++) {
+            if (new RegExp(regex[i]).test(password)) {
+                passed++;
+            }
+        }
+
+        var strength = "";
+        switch (passed) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+                strength = "<small class='progress-bar bg-danger' style='width: 40%'>Fraca</small>";
+                break;
+            case 4:
+                strength = "<small class='progress-bar bg-warning' style='width: 60%'>Média</small>";
+                break;
+            case 5:
+                strength = "<small class='progress-bar bg-success' style='width: 100%'>Forte</small>";
+                break;
+        }
+        password_strength.innerHTML = strength;
     }
 
-    //Regular Expressions.
-    var regex = new Array();
-    regex.push("[A-Z]"); //Uppercase Alphabet.
-    regex.push("[a-z]"); //Lowercase Alphabet.
-    regex.push("[0-9]"); //Digit.
-    regex.push("[$@$!%*#?&]"); //Special Character.
+    function checkPasswordMatch() {
+        var password = passwordInput.value;
+        var repeatPassword = repeatPasswordInput.value;
 
-    var passed = 0;
-
-    //Validate for each Regular Expression.
-    for (var i = 0; i < regex.length; i++) {
-      if (new RegExp(regex[i]).test(password)) {
-        passed++;
-      }
+        if (password !== repeatPassword) {
+            matchText.innerHTML = "<small class='progress-bar bg-danger' style='width: 100%'>As senhas não coincidem</small>";
+            return false;
+        } else {
+            matchText.innerHTML = "<small class='progress-bar bg-success' style='width: 100%'></small>";
+            return true;
+        }
     }
-
-    //Display status.
-    var strength = "";
-    switch (passed) {
-      case 0:
-      case 1:
-      case 2:
-        strength = "<small class='progress-bar bg-danger' style='width: 40%'>Fraca</small>";
-        break;
-      case 3:
-        strength = "<small class='progress-bar bg-warning' style='width: 60%'>Média</small>";
-        break;
-      case 4:
-        strength = "<small class='progress-bar bg-success' style='width: 100%'>Forte</small>";
-        break;
-
-    }
-    password_strength.innerHTML = strength;
-
-  }
+});
